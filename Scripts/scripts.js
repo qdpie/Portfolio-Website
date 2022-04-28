ef1d7e425decf99a6f38c5295fd28d1dea768531
Quentin <quentindouglas3@gmail.com>
2022-04-28 17:25:20 -0600

    Finishes

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -3,13 +3,26 @@ function scroll_home(){
 }
 
 function scroll_aboutMe(){
-    document.getElementById("About-me").scrollIntoView({behavior:'smooth'})
+    document.getElementById("About-me").scrollIntoView({behavior:'smooth'});
 }
 
 function scroll_projects(){
-    document.getElementById("Projects").scrollIntoView({behavior:'smooth'})
+    document.getElementById("Projects").scrollIntoView({behavior:'smooth'});
 }
 
 function scroll_contacts(){
-    document.getElementById("Footer").scrollIntoView({behavior:'smooth'})
+    document.getElementById("Footer").scrollIntoView({behavior:'smooth'});
+}
+
+
+function go_to_insta(){
+    location.href = "https://www.instagram.com/q.douglas_/";
+}
+
+function go_to_git(){
+    location.href = "https://github.com/qdpie";
+}
+
+function go_to_twitter(){
+    location.href = "https://twitter.com/QuentinhatesRed";
 }
